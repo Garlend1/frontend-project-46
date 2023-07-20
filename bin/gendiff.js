aa5990f63df2d3426f49e6328607cdfca8234533
garlend <garlend123@gmail.com>
2023-07-20 20:30:37 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,7 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .option('-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format<type/>', 'output format');
 
 program.parse();
