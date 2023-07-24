248fe6c96e15d2587c641f4c4fbfdf1b3d9695d8
garlend <garlend123@gmail.com>
2023-07-24 20:06:17 +0300

    step 3 update

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import genDiff from '../src/index.';
 
 program
-  .name('gendiff')
+  .name('genDiff')
   .description('Compares two configuration files and shows a difference.')
   .option('-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format<type/>', 'output format');
+  .option('-f, --format<type/>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
 
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+import fs from 'fs';
+import path from 'path';
+import process from 'process';
+
+const genDiff = (filepath1, filepath2) => {
+  const object1 = (fs.readFileSync(), path.resolve(), process.cwd(), _.trim());
+}
+
+export default genDiff;
