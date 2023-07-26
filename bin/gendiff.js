573d3ccc7cd8d11ee9d45adf24468dacf4d39d54
garlend <garlend123@gmail.com>
2023-07-26 19:15:28 +0300

    step 3 update

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
+
 import { program } from 'commander';
-import genDiff from '../src/index.';
+import genDiff from '../index.js';
 
 program
   .name('genDiff')
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
 import process from 'process';
 
 const genDiff = (filepath1, filepath2) => {
-  const object1 = (fs.readFileSync(), path.resolve(), process.cwd(), _.trim());
-}
-
+  const makeObject1 = fs.readFileSync(path.resolve(process.cwd(), filepath1));
+  const makeObject2 = fs.readFileSync(path.resolve(process.cwd(), filepath2));
+  return makeObject1;
+};
+console.log(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json'));
 export default genDiff;
