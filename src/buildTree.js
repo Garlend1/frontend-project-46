af72fe21bb2b5f79d9b271f042376e93cda1a813
garlend <garlend123@gmail.com>
2023-07-27 18:33:31 +0300

    step 3

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,8 +1,30 @@
 import _ from 'lodash';
 
 const makeTree = (data1, data2) => {
-  console.log(data1);
-  console.log(data2);
+  const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
+  const difference = {};
+  keys.forEach((key) => {
+    if (data1[key] !== data2[key]) {
+      difference[`- ${key}`] = data1[key];
+      difference[`+ ${key}`] = data2[key];
+    } else {
+      difference[`  ${key}`] = data1[key];
+    }
+  });
+
+  return JSON.stringify(difference, null, ' ').replace(/"|,/gi, '');
 };
 
 export default makeTree;
+
+// keys.forEach(key => {
+//   if (data1[key] !== data2[key]) {
+//     difference[`- ${key}`] = data1[key];
+//     difference[`+ ${key}`] = data2[key];
+//   } else {
+//     difference[`  ${key}`] = data1[key];
+//   }
+// });
+
+// return JSON.stringify(difference, null, ' ').replace(/"|,/gi, '');
+// };
