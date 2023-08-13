ca226e996a3011d85faa030e70c4682e7137d55a
garlend <garlend123@gmail.com>
2023-08-13 21:00:36 +0300

    step 8 fix

--- src/parsers.js
+++ src/parsers.js
@@ -5,6 +5,7 @@ const parsers = (data, format) => {
     case '.json':
       return JSON.parse(data);
     case '.yaml':
+    case '.yml':
       return yaml.load(data);
     default:
       throw new Error('Incorrect format');
